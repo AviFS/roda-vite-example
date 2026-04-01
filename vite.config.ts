d3603f5384625d7c58501bdfdc148e3a31450a9d
Avi Sternlieb <58723244+AviFS@users.noreply.github.com>
2026-03-31 17:14:40 -0700

    formatting

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   /* Restore Vite defaults */
   build: {
     emptyOutDir: true,
-    sourcemap: false,                                                              }
+    sourcemap: false,
+  }
 })
